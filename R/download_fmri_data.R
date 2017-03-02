3b0773a710a1d4407ccbf4c41d0c8c5c72249380
John Muschelli <muschellij2@gmail.com>
2017-03-02 16:05:31 +0000

    version 1.5.1

---
+++ R/download_fmri_data.R
@@ -0,0 +1,14 @@
+#' @title Download fMRI Image Filenames
+#'
+#' @description Download the files for the fMRI images
+#' @param ... arguments to pass to \code{\link{download_kirby21_data}},
+#' \code{modalities = "fMRI"} so it cannot be specified
+#' @return Indicator if the data is downloaded.
+#' 
+#' @export
+#' @importFrom kirby21.base download_kirby21_data all_modalities
+download_fmri_data =  function(...) {  
+  x = kirby21.base::download_kirby21_data(modality = "fMRI", ...)
+  return(x)
+}
+
